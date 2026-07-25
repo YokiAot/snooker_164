817f07141fbb05d0ec80077cd8416362e45fe3f9
YokiAot <thobutr_n@silpakorn.edu>
2026-07-25 11:55:52 +0700

    SetColor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,14 @@ public class GameManager : MonoBehaviour
     private int playerScore;
     public int PlayerScore { get { return playerScore; } set { playerScore = value; } }
 
+    [SerializeField]
+    private GameObject[] ballPositions;
+
+    [SerializeField]
+    private GameObject ballPrefab;
+
+    private static GameManager instance;
+
     public static GameManager Instance;
 
     private void Awake()
@@ -15,11 +23,25 @@ public class GameManager : MonoBehaviour
     }
     void Start()
     {
-      
+        SetBall(BallColor.Red, 1);
+        SetBall(BallColor.Yellow, 2);
+        SetBall(BallColor.Green, 3);
+        SetBall(BallColor.Brown, 4);
+        SetBall(BallColor.Blue, 5);
+        SetBall(BallColor.Pink, 6);
+        SetBall(BallColor.Black, 7);
     }
 
     void Update()
     {
         
+    }
+
+    private void SetBall(BallColor col, int i)
+    {
+       GameObject obj = Instantiate(ballPrefab, ballPositions[i].transform.position, Quaternion.identity);
+
+       Ball b = obj.GetComponent<Ball>();
+        b.SetColorAndPoint(col);
     }
 }
